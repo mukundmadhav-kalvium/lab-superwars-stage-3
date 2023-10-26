4dfa22bad47c2ec9bae66f7e7946048a1fafe839
mukundmadhav-kalvium <mukund.madhav@kalvium.community>
2023-10-26 12:03:26 +0530

    Superwars stage 3 Done

--- src/app.js
+++ src/app.js
@@ -23,13 +23,24 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
 
     // Instead of forloop use Map method
     // Code here
+    let detailedPlayers = players.map((players,index)=>{
+        let hero_or_villain=index%2==0 ? "hero" : "villain"
 
+        return {
+            name: players,
+            strength: getRandomStrength(),
+            type:hero_or_villain,
+            image: `images/super-${index + 1}.png`
+        }
+    });
+    
     return detailedPlayers;
 }
+
+
 
 // getting random strength
 const getRandomStrength = () => {
@@ -38,11 +49,20 @@ const getRandomStrength = () => {
 
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
-
+    let fragment=""
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+    fragment = players.filter(player=>player.type==type).map((players, index)=>{
+        return `<div class="player">
+                    <img src="${players.image}" alt="">
+                    <div class="name">${players.name}</div>
+                    <div class="strength"> ${players.strength}</div>
+                </div>`
+    }).join("");
+  
+ 
+
 
     return fragment;
 }
